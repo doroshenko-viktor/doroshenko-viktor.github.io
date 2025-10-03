6a9981958fa2e97ae91db325bcc72254f4548a1d
doroshenko-viktor <40042941+doroshenko-viktor@users.noreply.github.com>
2025-10-03 00:24:10 +0000

    Deploying to gh-pages from @ doroshenko-viktor/doroshenko-viktor.github.io@1700d902f42eff175b53847948f1e75d448b0bc9 🚀

--- _next/static/T_7nD6xFKIzBl_G_AkBxn/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Ffolders","\u002F","\u002Ffolders\u002F[...id]","\u002Fnotes\u002F[...noteKey]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/rEbIxoA2O-xiW5DUZwlG8/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Ffolders","\u002Fnotes\u002F[...noteKey]","\u002Ffolders\u002F[...id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
